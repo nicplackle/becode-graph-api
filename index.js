a8f8dfebb27dbf518a55864bb708b39362e597e0
nicplackle <nicplackle@gmail.com>
2019-07-31 12:07:56 +0200

    make it possible to save notes and display them in the dropdown menu

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const notes = document.querySelector("#notes");
 const editor = document.querySelector("#editor");
 
+// ----- load user's saved notes
+
+for (let i = 0; i < window.localStorage.length; i++) {
+  const newNote = document.createElement("option");
+  newNote.innerText = window.localStorage.key(i);
+  notes.appendChild(newNote);
+}
+changNote();
+
 // -------- check for empty not title
 
 function checkEmpty() {
@@ -15,4 +24,26 @@ function checkEmpty() {
 
 function saveNote() {
   window.localStorage.setItem(notes.nodeValue, editor.value);
+}
+
+// ----- change editor text to the currently selected note
+
+function changeNote() {
+  editor.value = window.localStorage.getItem(notes.value);
+}
+
+// ------ ask the user to name their new note, then create it
+// ------ add this note to the select div then focus to it
+
+function newNote() {
+  const note = prompt("Name of note?");
+  window.localStorage.setItem(note, "");
+
+  const noteElem = document.createElement("option");
+  noteElem.innerText = note;
+  notes.insertBefore(noteElem, notes.firstChild);
+
+  // focus this note
+  notes.value = note;
+  changeNote();
 }
